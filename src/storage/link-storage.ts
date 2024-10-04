752b74abd93e38fa18dbf19e5e97f7fa1715826d
Misterkingbad <davymichael20@gmail.com>
2024-10-04 16:48:41 -0300

    feat: :memo: metodos para obter e salvar item do app

---
+++ src/storage/link-storage.ts
@@ -0,0 +1,31 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const LINKS_STORAGE_KEY = "links-storage";
+
+type LinkStorage = {
+  id: string;
+  name: string;
+  url: string;
+  category: string;
+};
+
+const get = async (): Promise<LinkStorage[]> => {
+  const storage = await AsyncStorage.getItem(LINKS_STORAGE_KEY);
+  const response = storage ? JSON.parse(storage) : [];
+
+  return response;
+};
+
+const save = async (newLink: LinkStorage) => {
+  try {
+    const storage = await get();
+    const updated = JSON.stringify([...storage, newLink])
+
+    await AsyncStorage.setItem(LINKS_STORAGE_KEY, updated);
+  } catch (err) {
+    throw err;
+  }
+};
+
+
+export const linkStorage = { get, save }
