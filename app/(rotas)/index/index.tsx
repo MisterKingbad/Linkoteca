23ca21dcbb1c09e4e5079c4e3e1717e819bf0267
Misterkingbad <davymichael20@gmail.com>
2024-10-04 11:43:51 -0300

    feat: :memo: Modal de categoria

--- app/(rotas)/index/index.tsx
+++ app/(rotas)/index/index.tsx
@@ -1,37 +1,58 @@
-import React from "react"
-import { Image, View, TouchableOpacity, FlatList } from "react-native"
-import { MaterialIcons } from "@expo/vector-icons"
+import React from "react";
+import {
+  Image,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+  Text,
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
-import { styles } from "./styles"
-import { colors } from "@/src/styles/colors"
+import { styles } from "./styles";
+import { colors } from "@/src/styles/colors";
 
-import { Link } from "@/src/components/link"
-import { Categories } from "@/src/components/categories"
+import { Link } from "@/src/components/link";
+import { Categories } from "@/src/components/categories";
 
-export default function Home () {
+export default function Home() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Image source={require("@/src/assets/logo.png")} style={styles.logo}/>
+        <Image source={require("@/src/assets/logo.png")} style={styles.logo} />
 
         <TouchableOpacity>
-          <MaterialIcons name="add" size={32} color={colors.gray[100]}/>
+          <MaterialIcons name="add" size={32} color={colors.gray[100]} />
         </TouchableOpacity>
       </View>
-      <Categories/>
+      <Categories />
 
       <FlatList
         data={["1", "2", "3", "4", "5", "6"]}
-        keyExtractor={item => item}
+        keyExtractor={(item) => item}
         renderItem={() => (
-            <Link name="e" url="" onDetails={() => console.log("click")}/>
+          <Link name="e" url="" onDetails={() => console.log("click")} />
         )}
         style={styles.links}
         contentContainerStyle={styles.linksContent}
         showsVerticalScrollIndicator={false}
       />
-      
-    </View>
-  )
-}
 
+      <Modal visible={false} transparent>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Curso</Text>
+              <TouchableOpacity>
+                <MaterialIcons name="close" size={20} color={colors.gray[400]} />
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.modalLinkName}>MK</Text>
+
+            <Text style={styles.modalUrl}>MK</Text>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+}
--- app/(rotas)/index/styles.ts
+++ app/(rotas)/index/styles.ts
@@ -1,37 +1,68 @@
-import { StyleSheet } from "react-native"
-import { colors } from "@/src/styles/colors"
+import { StyleSheet } from "react-native";
+import { colors } from "@/src/styles/colors";
 
 export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      // justifyContent: "center",
-      // alignItems: "center"
-      paddingTop: 32
-    },
-    title: {
-      color: colors.gray[800],
-      fontSize: 22
-    },
-    header: {
-      paddingHorizontal: 24,
-      width: "100%",
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      marginBottom: 32
-    },
-    logo: {
-      height: 32,
-      width: 38,
-    },
-    links: {
-      borderTopWidth: 1,
-      borderTopColor: colors.gray[600]
-    },
-    linksContent: {
-      gap: 20,
-      padding: 24,
-      paddingBottom: 100
-    }
-  })
-  
+  container: {
+    flex: 1,
+    // justifyContent: "center",
+    // alignItems: "center"
+    paddingTop: 32,
+  },
+  title: {
+    color: colors.gray[800],
+    fontSize: 22,
+  },
+  header: {
+    paddingHorizontal: 24,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 32,
+  },
+  logo: {
+    height: 32,
+    width: 38,
+  },
+  links: {
+    borderTopWidth: 1,
+    borderTopColor: colors.gray[600],
+  },
+  linksContent: {
+    gap: 20,
+    padding: 24,
+    paddingBottom: 100,
+  },
+  modal: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  modalContent: {
+    backgroundColor: colors.gray[900],
+    borderTopWidth: 1,
+    borderTopColor: colors.gray[800],
+    paddingBottom: 42,
+    padding: 24,
+  },
+  modalHeader: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 32,
+  },
+  modalCategory: {
+    flex: 1,
+    fontSize: 16,
+    fontWeight: "500",
+    color: colors.gray[400],
+  },
+  modalLinkName: {
+    fontSize: 18,
+    fontWeight: "800",
+    color: colors.gray[200],
+  },
+  modalUrl: {
+    fontSize: 14,
+    color: colors.gray[400],
+  },
+});
